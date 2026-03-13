36082c675589239df780837128ad8882770f6f24
hailun <hailun@live.it>
2026-03-13 14:16:15 +0900

    Create llm.py

---
+++ utils/llm.py
@@ -0,0 +1,27 @@
+# ===============================
+# Prompt
+# ===============================
+
+def build_query_prompt(input_query):
+    prompt = f"""<|im_start|>system
+    你是一个中文专家，擅长将非疑问句或者词语转换成“A是什么？”这样的标准疑问句。<|im_end|>
+    <|im_start|>user
+    请将"{input_query}"转换成标准疑问句？
+    规则：①必须保留"{input_query}" ②加"？"结尾 ③不解释 ④不额外输出。
+    <|im_end|>
+    <|im_start|>assistant
+    """
+
+    return prompt
+
+
+def build_answer_prompt(input_query):
+    prompt = f"""<|im_start|>system
+            你是一名知识渊博的知识库专家。<|im_end|>
+            <|im_start|>user
+            请阅读下面的问句{input_query}，并给出长度在十个字之内的答案。
+            <|im_end|>
+            <|im_start|>assistant
+            """
+
+    return prompt
