a62c0ef2185c666e37ee52a7f605b1385b676699
hailun <hailun@live.it>
2026-03-13 15:17:19 +0900

    Update rag_db.py

--- utils/rag_db.py
+++ utils/rag_db.py
@@ -8,7 +8,7 @@ def rag_retrieval(query_vector, global_index, global_metadata, top_k=1):
     scores, indices = global_index.search(query_vector, top_k)
 
     idx = indices[0][0]
-
+    score = scores[0][0]
     if idx == -1:
         return None
-    return global_metadata[idx]["question"] + "\n\n" + global_metadata[idx]["answer"]
+    return score, global_metadata[idx]["question"] + "\n\n" + global_metadata[idx]["answer"]
