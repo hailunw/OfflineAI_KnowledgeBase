8ce146d4c28d7e841e0525c41525243646a3d871
hailun <hailun@live.it>
2026-03-13 15:05:48 +0900

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -4,9 +4,9 @@ from pathlib import Path
 # 全局变量
 # ===============================
 
-DOCS_DIR = Path(r"C:\Users\shuei\Desktop\OfflineAI_KnowledgeBase\docs")
-llm_model_path = r"C:\Users\shuei\Desktop\OfflineAI_KnowledgeBase\llm_qwen\qwen2.5-1.5b-instruct.Q4_K_M.gguf"
-sentence_transform_model = r"C:\Users\shuei\Desktop\OfflineAI_KnowledgeBase\paraphrase-MiniLM"
+DOCS_DIR = Path("docs")
+llm_model_path = "llm_qwen\qwen2.5-1.5b-instruct.Q4_K_M.gguf"
+rag_tool_model_path = "paraphrase-MiniLM"
 FAISS_DIR = "faiss_rag"
 INDEX_PATH = f"{FAISS_DIR}/index.bin"
 META_PATH = f"{FAISS_DIR}/metadata.json"
